f4fe42dee89b7e32a9af079661efa6a4514a12e2
Mattia Romeo <mattiaromeo@gmail.com>
2017-11-14 14:51:49 +0100

    started story 2

--- src/main/java/be/cegeka/selfeval5/domain/Incident.java
+++ src/main/java/be/cegeka/selfeval5/domain/Incident.java
@@ -1,4 +1,45 @@
 package be.cegeka.selfeval5.domain;
 
+import javax.inject.Named;
+import javax.persistence.*;
+
+@Entity
+@Table(name="INCIDENT")
 public class Incident {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
+    private int id;
+    @Column(name="NAME")
+    private String name;
+    @Column(name="TYPE")
+    private String type;
+    @Column(name="DISTANCE")
+    private double distance;
+
+    public Incident() {
+    }
+
+    public Incident(String name, String type, double distance) {
+        this.name = name;
+        this.type = type;
+        this.distance = distance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
 }
--- src/main/resources/db/migration/V1__createTables.sql
+++ src/main/resources/db/migration/V1__createTables.sql
@@ -8,3 +8,11 @@ id int primary key auto_increment,
 name varchar(100),
 distance double
 );
+
+create table INCIDENT(
+id int primary key auto_increment,
+name varchar(100),
+type varchar(100),
+distance double,
+highwayid int
+);
